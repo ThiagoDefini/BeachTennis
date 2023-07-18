75b9686476ec28798c0572bbe267d758c34681d1
ThiagoDefini <thiagod.scherer@hotmail.com>
2023-07-18 16:08:02 -0300

    Adicionado o projeto

---
+++ BeachTennis/BeachTennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachTennisApp.swift
+//  BeachTennis
+//
+//  Created by Thiago Defini on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BeachTennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
