6b0822ee9820aa35efc35c6d40956276805b2f18
maduMmaciel <117127947+maduMmaciel@users.noreply.github.com>
2023-08-09 16:04:55 -0300

    mudança no componente da tela de informações do torneio

---
+++ BeachTennis/WatchOS/WatchOSUI.swift
@@ -0,0 +1,20 @@
+//
+//  WatchOSUI.swift
+//  BeachTennis
+//
+//  Created by Madu Maciel on 08/08/23.
+//
+
+import SwiftUI
+
+struct WatchOSUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WatchOSUI_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchOSUI()
+    }
+}
